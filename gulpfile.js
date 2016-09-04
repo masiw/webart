9afb5687157bae04c57b971cb4100d9dca5e8d81
masi <masi.woermann@gmail.com>
2016-09-04 21:07:29 +0100

    added styles less preprocessor.

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,31 @@ let gulp = require('gulp')
   , babel = require('gulp-babel')
   , eslint = require('gulp-eslint')
   , concat = require('gulp-concat')
-  , del = require('del')
+  , less = require('gulp-less')
 //  , sourcemaps = require('gulp-sourcemaps')
+, del = require('del')
 
 
-
-gulp.task('default', ['clean', 'lint', 'test', 'build'], function() {
+gulp.task('default', ['clean', 'lint', 'test','less', 'build'], function() {
 
 })
 
 gulp.task('build', function() {
-
+  
   return gulp.src(['src/*.js'])
     .pipe(babel())
     .pipe(concat('webart.js'))
     .pipe(gulp.dest('dist'))
 })
 
+gulp.task('less', function() {
+
+  return gulp.src('./src/*.less')
+    .pipe(less())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('dist'))
+})
+	  
 gulp.task('test', function() {
 
   return gulp.src(['./test/*.test.js'], { read: false })
